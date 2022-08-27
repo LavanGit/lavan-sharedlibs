b56b4b3b3e1b07f4f170dcf1be67468b0e47c73b
LavanGit <109799614+LavanGit@users.noreply.github.com>
2022-08-27 12:33:56 +0530

    Create tomcatdeploy.groovy

---
+++ vars/tomcatdeploy.groovy
@@ -0,0 +1,12 @@
+def call(ip,user,credid){
+  sshagent( [credid]) {
+    sh "mv target/myweb*.war target/app.war"
+    // copy war
+    sh "scp -o StrictHostKeyChecking=no target/app.war ec2-user@${ip}:/opt/tomcat9/webapps"
+    // stop tomcat
+    sh "ssh ${user}@${ip} /opt/tomcat9/bin/shutdown.sh"
+    // start tomcat
+    sh "ssh ${user}@${ip}/opt/tomcat9/bin/startup.sh"
+  }
+}
+  
